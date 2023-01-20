2802f827043b18bb97dd6f0566d3b9afdb4105d9
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-20 09:10:17 -0800

    setup dev environment

--- Parks/Models.cs/Type.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections.Generic;
-namespace Parks.Models;
-
-public class ParkType
-{
-  public int ParkTypeId { get;set; }
-  public string Type { get;set; }
-  public List<Park> Parks { get; set; }
-}
--- Parks/Program.cs
+++ Parks/Program.cs
@@ -1,8 +1,22 @@
+using Parks.Models;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+// Use MySql 
+builder.Services.AddDbContext<ParksContext>(
+                  dbContextOptions => dbContextOptions
+                    .UseMySql(
+                      builder.Configuration["ConnectionStrings:DefaultConnection"], 
+                      ServerVersion.AutoDetect(builder.Configuration["ConnectionStrings:DefaultConnection"]
+                    )
+                  )
+                );
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
