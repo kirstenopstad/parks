7398282a185987349c62f306e6560882979a3ece
Kirsten Opstad <kirsten.opstad@gmail.com>
2023-01-21 17:09:02 -0800

    add GET to Client, via API v1 (no auth req)

--- ParksApi/Controllers/v1/ParksContoller.cs
+++ ParksApi/Controllers/v1/ParksContoller.cs
@@ -9,7 +9,7 @@ namespace ParksApi.Controllers.v1;
 [Route("api/v{version:apiVersion}/[controller]")]
 [ApiVersion("1.0")]
 [ApiController]
-[Authorize]
+// [Authorize]
 public class ParksController : ControllerBase
 {
   private readonly ParksApiContext _db;
--- ParksClient/Models/ApiHelper.cs
+++ ParksClient/Models/ApiHelper.cs
@@ -8,7 +8,7 @@ public class ApiHelper
     public static async Task<string> GetAll()
     {
       RestClient client = new RestClient("http://localhost:5000/");
-      RestRequest request = new RestRequest($"api/parks", Method.Get);
+      RestRequest request = new RestRequest($"api/v1/parks", Method.Get);
       RestResponse response = await client.GetAsync(request);
       return response.Content;
     }
